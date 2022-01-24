38aa0182dcc3a9672ab89a2a532add5fa29ad1cb
ChangHwan Kim <raccoonste@naver.com>
2022-01-24 23:36:37 +0900

    Level

---
+++ Level1/FlipArray.java
@@ -0,0 +1,26 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class FlipArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FlipArray fa = new FlipArray();
+		System.out.println(Arrays.toString(fa.solution(12347)));
+	}
+
+	public int[] solution(long n) {
+		String str = "";
+		str += n;
+		int[] answer = new int[str.length()];
+		int cnt = 0;
+		while (n > 0) {
+			answer[cnt] = (int) (n % 10);
+			n /= 10;
+			cnt++;
+		}
+		return answer;
+	}
+
+}
