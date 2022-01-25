6dc8b392a1f438ec52de47e4906958d22e36cca2
ChangHwan Kim <raccoonste@naver.com>
2022-01-25 17:29:00 +0900

    Level1

--- Level1/EvenAndOdd.java
+++ Level1/EvenAndOdd.java
@@ -1,22 +1,22 @@
 package Level1;
 
 public class EvenAndOdd {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		EvenAndOdd eo = new EvenAndOdd();
-		int num = 100;
-		System.out.println(eo.solution(num));
+		System.out.println(eo.solution2(100));
 	}
-    public String solution(int num) {
-        String answer = "";
-        if(num%2==0){
-            answer = "Even";
-        }
-        else{
-            answer = "Odd";
-        }
-        
-        return answer;
-    }
+
+	public String solution1(int num) {
+		return num % 2 == 0 ? "Even" : "Odd";
+	}
+
+	public String solution2(int num) {
+		String answer = "";
+		if (num % 2 == 0) {
+			answer = "Even";
+		} else {
+			answer = "Odd";
+		}
+		return answer;
+	}
 }
