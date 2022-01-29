8de4a6b40323b520712caee375d7f6e0b1eb4177
ChangHwan Kim <raccoonste@naver.com>
2022-01-29 23:08:38 +0900

    Level1

---
+++ Level1/StringInterger.java
@@ -0,0 +1,14 @@
+package Level1;
+
+public class StringInterger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringInterger si = new StringInterger();
+		System.out.println(si.solution("-1234"));
+	}
+	public int solution(String s) {
+		int answer = 0;
+		return answer = Integer.parseInt(s);
+	}
+}
