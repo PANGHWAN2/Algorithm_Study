5c5f1e83f21da5160b3da8dd901e64fd1663697b
ChangHwan Kim <raccoonste@naver.com>
2022-02-13 22:43:26 +0900

    Level1

--- Level1/FindKim.java
+++ Level1/FindKim.java
@@ -8,17 +8,17 @@ public class FindKim {
 		FindKim fk = new FindKim();
 		String[] seoul = { "Jane", "Park", "Kim" };
 		System.out.println(fk.solution(seoul));
-		
+
 	}
 
 	public String solution(String[] seoul) {
 		String answer = "";
 		for (int i = 0; i < seoul.length; i++) {
-			//if (seoul[i] == "Kim") {
-			if(seoul[i].equals("Kim")) {	
-            answer = "김서방은 " + i + "에 있다";
+			// if (seoul[i] == "Kim") {
+			if (seoul[i].equals("Kim")) {
+				answer = "김서방은 " + i + "에 있다";
 			}
-		}    
+		}
 		return answer;
 	}
 }
