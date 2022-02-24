dec355bb274f196df1d6e869dc2291fb36667a9c
ChangHwan Kim <raccoonste@naver.com>
2022-02-24 23:37:54 +0900

    Level1

--- Level1/DescendingOrder.java
+++ Level1/DescendingOrder.java
@@ -8,7 +8,7 @@ public class DescendingOrder {
 		// TODO Auto-generated method stub
 
 		DescendingOrder DO = new DescendingOrder();
-		System.out.println(DO.solution(118372));
+		System.out.println(DO.solution(79786416));
 	}
     public long solution(long n) {
         long answer = 0;
@@ -26,5 +26,46 @@ public class DescendingOrder {
 		answer = Long.parseLong(str);
         return answer;
     }
+
+	public long solution2(long n) {
+		long answer = 0;
+		int ten = 1;
+		long tmp_num = n;
+		int count = 0;
+		int i = 0;
+
+		while (tmp_num != 0)
+		{
+			count++;
+			tmp_num = tmp_num / 10;
+		}
+
+		tmp_num = n;
+		int arr[] = new int[count];
+
+		while (tmp_num != 0)
+		{
+			arr[i] = (int) (tmp_num % 10);
+			tmp_num = tmp_num / 10;
+			i++;
+		}
+
+		for (i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					int tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+
+		for (i = arr.length - 1; i >= 0; i--)
+		{
+			answer += arr[i] * ten;
+			ten *= 10;
+		}
+
+		return answer;
+	}
 }
-    
