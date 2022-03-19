a57706e8a087784e0e559241978023848a1a6d24
ChangHwan Kim <raccoonste@naver.com>
2022-03-20 03:40:31 +0900

    20044

--- BAEKJOON/ProjectTeams.java
+++ BAEKJOON/ProjectTeams.java
@@ -1,16 +1,24 @@
 package BAEKJOON;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ProjectTeams {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
-		
-		long Ability[] = new long[2 * num]; 
-
-		for (int i = 0; i < (2 * num); i++) {
+		int Ability[] = new int[num * 2];
+		for (int i = 0; i < (num * 2); i++) {
 			Ability[i] = scan.nextInt();
 		}
+		Arrays.sort(Ability);
+
+		int answers[] = new int[num];
+		for (int i = 0; i < num; i++) {
+			answers[i] = Ability[i] + Ability[Ability.length - i - 1];
+		}
+		// System.out.println(Arrays.toString(answer));
+		Arrays.sort(answers);
+		System.out.println(answers[0]);
 	}
 }
