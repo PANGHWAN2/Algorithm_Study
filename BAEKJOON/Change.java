fd81042c6a4e8d82243e3967a222779e9dc61400
ChangHwan Kim <raccoonste@naver.com>
2022-03-28 23:11:23 +0900

    5585

--- BAEKJOON/Change.java
+++ BAEKJOON/Change.java
@@ -6,16 +6,16 @@ public class Change {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		n = 1000 - n;
+		int num = sc.nextInt();
+		num = 1000 - num;
 		int answer = 0;
 		int[] yen = { 500, 100, 50, 10, 5, 1 };
 		for (int i = 0; i < 6; i++) {
-			if (yen[i] > n) {
+			if (yen[i] > num) {
 				continue;
 			}
-			answer += n / yen[i];
-			n %= yen[i];
+			answer += num / yen[i];
+			num %= yen[i];
 
 		}
 		System.out.println(answer);
