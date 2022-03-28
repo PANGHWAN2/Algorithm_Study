d509c22a2e2cedcff9343f9ab537c13933eca507
ChangHwan Kim <raccoonste@naver.com>
2022-03-28 22:34:08 +0900

    2847

--- BAEKJOON/DongJunsGame.java
+++ BAEKJOON/DongJunsGame.java
@@ -9,13 +9,15 @@ public class DongJunsGame {
 		int answer = 0;
 		int n = sc.nextInt();
 		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
+		
+		for (int i = 0; i < n; i++) 
 			arr[i] = sc.nextInt();
-		for (int i = n - 1; i > 0; i--)
+		for (int i = n - 1; i > 0; i--) {
 			while (arr[i] <= arr[i - 1]) {
 				arr[i - 1]--;
 				answer++;
 			}
+		}
 		System.out.println(answer);
 	}
 }
