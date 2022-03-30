f565f0b82629938fd85a71ba6dbe07236ac2d46c
ChangHwan Kim <raccoonste@naver.com>
2022-03-30 20:52:51 +0900

    14659

---
+++ BAEKJOON/HanzoShimada.java
@@ -0,0 +1,33 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class HanzoShimada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int answer = 0;
+		int max = arr[0];
+		int cnt = 0;
+		for(int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			if (max < arr[i]) {
+				answer = Math.max(answer,cnt);
+				max = arr[i];
+				cnt = 0;
+				
+			}
+			else 
+				cnt++;	
+		}
+		System.out.println(answer);
+		System.out.println(cnt);
+		//answer = Math.max(answer,cnt);
+		
+		System.out.println(answer);	
+	}
+}
