160c747edc30ef6b6ae3d5aba4da82f2dca79d90
ChangHwan Kim <raccoonste@naver.com>
2022-05-02 22:40:25 +0900

    20220502

--- BAEKJOON/MailTest.java
+++ BAEKJOON/MailTest.java
@@ -4,19 +4,25 @@ public class MailTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String pw= "";
+		MailTest mt = new MailTest();
+		mt.getPw();
 
+	}
 
-	        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-	                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+	public void getPw() {
+		// TODO Auto-generated method stub
 
-	        String str = "";
+		char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+				'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-	        int idx = 0;
-	        for (int i = 0; i < 10; i++) {
-	            idx = (int) (charSet.length * Math.random());
-	            str += charSet[idx];
-	        }
-	    }
-	
+		String str = "";
 
+		int idx = 0;
+		for (int i = 0; i < 10; i++) {
+			idx = (int) (charSet.length * Math.random());
+			str += charSet[idx];
+		}
+		System.out.println(str);
+	}
 }
