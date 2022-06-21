df9745e36825fa33b106c4ac5efa17e8cecb2aa1
ChangHwan Kim <raccoonste@naver.com>
2022-06-21 23:51:43 +0900

    mo

---
+++ BAEKJOON/A_B.java
@@ -0,0 +1,17 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class A_B {
+
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A-B);
+ 
+		in.close();
+	}
+}
