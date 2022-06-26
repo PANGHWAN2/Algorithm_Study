6218b9476c367ee2b31997e7077262169143c7c6
ChangHwan Kim <raccoonste@naver.com>
2022-06-26 23:31:02 +0900

    review

---
+++ BAEKJOON/Cat.java
@@ -0,0 +1,13 @@
+package BAEKJOON;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
