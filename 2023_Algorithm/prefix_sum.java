9de17c206337d1fbd18a2cee6fec37e1091a6d60
ChangHwan Kim <raccoonste@naver.com>
2023-02-03 17:37:54 +0900

    prefix

--- 2023_Algorithm/prefix_sum.java
+++ 2023_Algorithm/prefix_sum.java
@@ -49,3 +49,34 @@ public class Main {
 	}
 			
 }
+
+//구간합
+//Python
+// array = [1, 8, 7, 4, 3, 5, 6]
+// n = len(array)
+// prefix_sum = [0] * (n + 1)
+// x, y = 1, 5
+
+// for i in range(n):
+//     prefix_sum[i + 1] = prefix_sum[i] + array[i]
+        
+// part_sum = prefix_sum[y] - prefix_sum[x - 1]
+
+//Java
+public class Main {
+
+	public static void main(String[] args){
+		int[] array = {1, 8, 7, 4, 3, 5, 6};
+		int n = array.length;
+		int[] prefix_sum = new int[n + 1];
+		int x = 1;
+		int y = 5;
+		
+		for(int i = 0; i < n; i++) {
+			prefix_sum[i + 1] = prefix_sum[i] + array[i];
+		}
+		
+		int part_sum = prefix_sum[y] - prefix_sum[x - 1];
+	}
+			
+}
