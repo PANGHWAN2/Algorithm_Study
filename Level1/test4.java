6fa6e463a8d2806103ad594d427c864092ec6efe
ChangHwan Kim <raccoonste@naver.com>
2023-02-15 22:13:14 +0900

    java reivew

---
+++ Level1/test4.java
@@ -0,0 +1,33 @@
+package Level1;
+
+import java.util.Arrays;
+
+public class test4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 8;
+		
+		System.out.println(solution(n));
+	}
+	public static long solution(int n) {
+		long answer = 1 ;
+		int s = 3;
+		int q = n/s;
+		int r = n% s;
+		
+		int[] arr= new int [s];
+		for(int i = 0 ; i < s; i++) {
+			if(s - r == i) {
+				q++;
+			}
+			arr[i] = q;
+			
+			answer *= arr[i];
+		}
+		
+		
+		
+		return answer;
+		
+	}
+}
